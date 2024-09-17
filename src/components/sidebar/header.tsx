18b8660861f382bf1ea043afb175a305759d9652
AthithianV <athithianpkt2001@gmail.com>
2024-09-17 18:10:34 +0530

    Created Siderbar

---
+++ src/components/sidebar/header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='w-1/6 bg-primary'>
+
+    </div>
+  )
+}
+
+export default Header;
---
+++ src/components/sidebar/sidebar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './header';
+
+const Sidebar = () => {
+  return (
+    <div className='w-9/10'>
+      <Header/>
+    </div>
+  )
+}
+
+export default Sidebar;
